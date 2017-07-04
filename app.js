a2d48618159ca1704c233f3bcc926dbd07890ca9
Tony Scapardine <arscapa@gmail.com>
2017-07-03 23:12:58 -0500

    Addded mongoose connection

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ var users = require('./routes/users');
 
 var app = express();
 
+//Setup mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = "mongodb://<admin>:<admin>@ds147979.mlab.com:47979/local_library";
+monggose.connect(mongoDB);
+var db = mongoose.connection;
+db.on('error',console.error.bind(console, "MongoDB connection error:'));
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
