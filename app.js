736b5260336161b58bed07901ca7a590055039ed
Tony Scapardine <arscapa@gmail.com>
2017-07-24 14:44:38 -0500

    Resolved bugs with routes and controllers

--- app.js
+++ app.js
@@ -7,15 +7,16 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var catalog = require('./routes/catalog');  //Import routes for "catalog" area of site
 
 var app = express();
 
 //Setup mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = "mongodb://<admin>:<admin>@ds147979.mlab.com:47979/local_library";
-monggose.connect(mongoDB);
+var mongoDB = "mongodb://admin:admin@ds147979.mlab.com:47979/local_library";
+mongoose.connect(mongoDB, {useMongoClient: true});
 var db = mongoose.connection;
-db.on('error',console.error.bind(console, "MongoDB connection error:'));
+db.on('error',console.error.bind(console, 'MongoDB connection error:'));
 
 
 // view engine setup
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/catalog', catalog);  // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,44 +1,41 @@
 var Author = require('../models/author');
 
-//Display list of all Authors
-exports.author_list = function(req,res) {
-	res.send('NOT IMPLEMENTED: Author list')
+// Display list of all Authors
+exports.author_list = function(req, res) {
+    res.send('NOT IMPLEMENTED: Author list');
 };
 
-//Display detail page for a specific Author
-exports.author_detail = function(req,res) {
-	res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
-	};
-	
+// Display detail page for a specific Author
+exports.author_detail = function(req, res) {
+    res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+};
 
-//Display Author create form on GET
+// Display Author create form on GET
 exports.author_create_get = function(req, res) {
-	res.send('NOT IMPLEMENTED: Author create GET');
+    res.send('NOT IMPLEMENTED: Author create GET');
 };
 
 // Handle Author create on POST
 exports.author_create_post = function(req, res) {
-	res.send('NOT IMPLEMENTED: Author create POST');
+    res.send('NOT IMPLEMENTED: Author create POST');
 };
 
-//Display Author delete form on GET
+// Display Author delete form on GET
 exports.author_delete_get = function(req, res) {
-		res.send('NOT IMPLEMENTED: Author delete GET');
+    res.send('NOT IMPLEMENTED: Author delete GET');
 };
 
-//Handle Author delete on POST
+// Handle Author delete on POST
 exports.author_delete_post = function(req, res) {
-	res.send('NOT IMPLEMENTED: Author delete POST');
+    res.send('NOT IMPLEMENTED: Author delete POST');
 };
 
-//Display Author update form on GET
+// Display Author update form on GET
 exports.author_update_get = function(req, res) {
-	res.send('NOT IMPLEMENTED: Author update GET');
+    res.send('NOT IMPLEMENTED: Author update GET');
 };
 
-//Handle Author update on POST
+// Handle Author update on POST
 exports.author_update_post = function(req, res) {
-	res.send('NOT IMPLEMENTED: Author udpate POST');
+    res.send('NOT IMPLEMENTED: Author update POST');
 };
-
-
